700507192efa93571239dc705cf75b691ee61b03
sccn <2043017025@qq.com>
2019-04-04 16:30:29 +0800

    45级

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -3,6 +3,7 @@ import 'package:provide/provide.dart';
 import '../provide/details_info.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import './details_page/details_top_area.dart';
+import './details_page/details_explain.dart';
 
 class DetailsPage extends StatelessWidget {
   final String goodsId;
@@ -33,6 +34,7 @@ class DetailsPage extends StatelessWidget {
               child: Column(
                 children: <Widget>[
                   DetailsTopArea(),
+                  DetailsExplain()
                 ],
               ),
             );
