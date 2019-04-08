81e8fdcc24ef55efdeb99e8401a5734e81290c6c
sccn <2043017025@qq.com>
2019-04-08 22:26:32 +0800

    46更新

--- lib/provide/details_info.dart
+++ lib/provide/details_info.dart
@@ -4,7 +4,24 @@ import '../service/service_method.dart';
 import 'dart:convert';
 
 class DetailsInfoProvide with ChangeNotifier{
-   DetailsModel goodsInfo = null;
+  DetailsModel goodsInfo = null;
+
+  bool isLeft = true;
+  bool isRight = false;
+
+  //改变tabBar的状态
+  changeLeftAndRight(String changeState){
+    if(changeState=='left'){
+      isLeft=true;
+      isRight=false;
+    }else{
+      isLeft=false;
+      isRight=true;
+    }
+     notifyListeners();
+  }
+
+
   //从后台获取商品信息
   getGoodsInfo(String id ){
     var formData = { 'goodId':id, };
